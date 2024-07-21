89b136163d1837e95c87bd249d79d1d912c03ebc
Pearlicia <suonghbts2302004@fpt.edu.vn>
2024-07-21 16:26:45 +0700

    7/21 adjust logo
    7/21 adjust logo

--- src/ultil/icons.js
+++ src/ultil/icons.js
@@ -8,7 +8,6 @@ import {
     IoInformationCircleOutline,
 } from "react-icons/io5";
 import { IoIosSearch, IoMdTime, IoMdClose } from "react-icons/io";
-
 import { FaRegEye, FaUserCircle, FaRegStar, FaRegUser, FaRegNewspaper, FaPlus, FaCartPlus, FaMinusCircle } from "react-icons/fa";
 import { BsCurrencyDollar } from "react-icons/bs";
 import { GrPrevious, GrNext } from "react-icons/gr";
@@ -18,15 +17,10 @@ import { FiFlag } from "react-icons/fi";
 import { MdOutlineChildCare, MdAccessTime } from "react-icons/md";
 import { TbMessage2Up } from "react-icons/tb";
 import { RiContactsBook3Line, RiMoneyDollarCircleLine  } from "react-icons/ri";
-import { DiHackernews } from "react-icons/di";
 import { CiForkAndKnife } from "react-icons/ci";
 import { BiSolidDrink } from "react-icons/bi";
 import { GiChickenOven } from "react-icons/gi";
 import { FaBowlRice } from "react-icons/fa6";
-
-
-
-
 
 const icons = {
 
@@ -72,8 +66,6 @@ const icons = {
 
     //news
     FaRegNewspaper,
-    //news type
-    DiHackernews,
 
     //menu
     CiForkAndKnife,
