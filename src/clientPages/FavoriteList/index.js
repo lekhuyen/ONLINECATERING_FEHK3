cdd2759a0a58054608a7e545086b7cac62e3f9f1
Pearlicia <suonghbts2302004@fpt.edu.vn>
2024-07-24 14:58:49 +0700

    7/24 UI favlist
    design

---
+++ src/clientPages/FavoriteList/index.js
@@ -0,0 +1,60 @@
+import styles from './Favorite.module.scss'
+import { images } from "../../constants/index.js";
+import React from 'react';
+
+function FavoriteList(props) {
+  return (
+    <div className={styles.user_favorite_all}>
+      <div className={styles.user_favorite_title}>
+        <h3>Your Favorite List</h3>
+      </div>
+      <div className={styles.user_favorite_row}>
+        <div className={styles.user_favorite_left} >
+          <img src={images.SurfnTurf} className={styles.user_favorite_images} />
+        </div>
+        <div className={styles.user_favorite_right} >
+          <p>
+            <span className={styles.user_favorite__right_title}>Favorite Dish: Surf & Turf</span>
+          </p>
+          <p>
+            <span className={styles.user_favorite_text}>
+              Sometimes called reef and beef, is a main course combining seafood and red meat, typically beefsteak
+            </span>
+          </p>
+        </div>
+      </div>
+      <div className={styles.user_favorite_row}>
+        <div className={styles.user_favorite_left} >
+          <img src={images.dumaloo} className={styles.user_favorite_images} />
+        </div>
+        <div className={styles.user_favorite_right} >
+          <p>
+            <span className={styles.user_favorite__right_title}>Favorite Dish: Dum Aloo</span>
+          </p>
+          <p>
+            <span className={styles.user_favorite_text}>
+              Dum aloo or aloor dum or aloo dum is a potato-based curry dish
+            </span>
+          </p>
+        </div>
+      </div>
+      <div className={styles.user_favorite_row}>
+        <div className={styles.user_favorite_left} >
+          <img src={images.applewalnut} className={styles.user_favorite_images} />
+        </div>
+        <div className={styles.user_favorite_right} >
+          <p>
+            <span className={styles.user_favorite__right_title}>Favorite Dish: Apple Walnut Salad</span>
+          </p>
+          <p>
+            <span className={styles.user_favorite_text}>
+            Crisp apples, toasty walnuts and creamy feta or blue cheese make this apple walnut salad ideal for holidays and parties
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FavoriteList;
